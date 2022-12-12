16e7061fcfa05a4f092db32b3f20ed352547fbaa
Iconer <yoyofa2@hotmail.com>
2022-12-13 02:30:59 +0900

    텍스쳐 육면체와 텍스쳐 평면 클래스 추가

---
+++ boxhead/boxhead/TextureCubeModel.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+class TextureCubeModel : public Model
+{
+public:
+	constexpr TextureCubeModel(std::string_view name, ogl::VertexStream::Buffer& buffer)
+		: Model(name, buffer)
+		, textureID(0)
+	{}
+
+	void PrepareRendering(ogl::Uniform& tex_uniform) const
+	{
+		Model::PrepareRendering();
+
+		tex_uniform.Assign(textureID);
+	}
+
+	virtual void Render() const override
+	{
+		for (GLint i = 0; i < 6; i++)
+		{
+			ogl::Render(ogl::PRIMITIVE_TYPES::TRIANGLE_FAN, 4, i * 4);
+		}
+	}
+
+	static constexpr size_t GetID()
+	{
+		return ReferenceIndex;
+	}
+
+	GLint textureID;
+
+	constinit inline static size_t ReferenceIndex = 4;
+};
---
+++ boxhead/boxhead/TexturePlaneModel.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+class TexturePlaneModel : public Model
+{
+public:
+	constexpr TexturePlaneModel(std::string_view name, ogl::VertexStream::Buffer& buffer)
+		: Model(name, buffer)
+		, textureID(0)
+	{}
+
+	void PrepareRendering(ogl::Uniform& tex_uniform) const
+	{
+		Model::PrepareRendering();
+
+		tex_uniform.Assign(textureID);
+	}
+
+	virtual void Render() const override
+	{
+		ogl::Render(ogl::PRIMITIVE_TYPES::TRIANGLE_FAN, 4);
+	}
+
+	static constexpr size_t GetID()
+	{
+		return ReferenceIndex;
+	}
+
+	GLint textureID;
+
+	constinit inline static size_t ReferenceIndex = 3;
+};
+
