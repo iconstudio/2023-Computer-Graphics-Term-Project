2638b74e51db1dab357459fba60f5df6174b1481
Iconer <yoyofa2@hotmail.com>
2022-12-13 19:10:19 +0900

    최적화 중

--- boxhead/boxhead/WorldManager.cpp
+++ boxhead/boxhead/WorldManager.cpp
@@ -1,2 +1,119 @@
 #include "pch.hpp"
 #include "WorldManager.hpp"
+#include "Framework.hpp"
+
+void WorldManager::Awake()
+
+{
+	// 딱 한번만 높이 맵 생성
+	std::fstream stage_file{ stageFilepath, std::ios::in };
+
+	if (stage_file)
+	{
+		terrainData = new int* [boardSizeH];
+		for (size_t y = 0; y < boardSizeW; y++)
+		{
+			terrainData[y] = new int[boardSizeW];
+		}
+
+		char piece{};
+		size_t x = 0, y = 0;
+
+		while (stage_file >> piece)
+		{
+			if (' ' == piece) continue;
+
+			SetTerrainAt(x, y, int(piece - '0'));
+
+			x++;
+			if (boardSizeW <= x)
+			{
+				x = 0;
+				y++;
+			}
+		}
+
+		for (size_t i = 0; i < boardSizeW; i++)
+		{
+			for (size_t j = 0; j < boardSizeH; j++)
+			{
+				// 열 우선으로 삽입
+				auto& terrain_cell = GetTerrainAt(i, j);
+
+				if (0 < terrain_cell)
+				{
+					float cell_height = 1.0f * float(terrain_cell);
+					heightMap.emplace_back(i, j, cell_height);
+				}
+			}
+		}
+	}
+}
+
+void WorldManager::Start(Scene* scene)
+{
+	// 모델 가져오기
+	auto wall_model_view = ModelView::GetReference<SideCubeModel>();
+
+	// 높이 맵의 내용대로 벽 생성
+	for (auto& height_block : heightMap)
+	{
+		const float cheight = boardScaleH * static_cast<float>(height_block.myHeight);
+		const float cx = boardScaleW * static_cast<float>(height_block.x);
+		const float cy = 0.5f * cheight;
+		const float cz = boardScaleD * static_cast<float>(height_block.y);
+
+		Entity* wall = scene->CreateEntity<Entity>(wall_model_view, cx, cy, cz);
+		wall->Scale(boardScaleW, cheight, boardScaleD);
+	}
+
+	// 타일 텍스쳐 가져오기
+	GLint ground_textures[6]{};
+
+	ground_textures[0] = Framework::GetTexture(0); // ("Dirt 0");
+	ground_textures[1] = Framework::GetTexture(1); // ("Dirt 0");
+	ground_textures[2] = Framework::GetTexture(2); // ("Dirt 0");
+	ground_textures[3] = Framework::GetTexture(3); // ("Dirt 0");
+	ground_textures[4] = Framework::GetTexture(4); // ("Dirt 0");
+	ground_textures[5] = Framework::GetTexture(5); // ("Dirt 0");
+
+	for (size_t i = 0; i < tileCountH; i++)
+	{
+		for (size_t j = 0; j < tileCountW; j++)
+		{
+			auto& tile = tileData[i][j];
+
+			const float cx = static_cast<float>(j) * boardScaleW * 2;
+			const float cy = static_cast<float>(i) * boardScaleD * 2;
+
+			tile.worldMatrix = glm::scale(glm::translate(ogl::identity, { cx, 0.0f, cy }), glm::vec3{ 4.0f });
+			tile.textureID = ground_textures[(::rand() % 6)];
+
+			tileMap.push_back(tile);
+		}
+
+		delete tileData[i];
+	}
+
+	delete& tileData;
+}
+
+void WorldManager::Render(ModelView model, ogl::Uniform& world_uniform, ogl::Uniform& texture_uniform)
+{
+	for (size_t i = 0; i < tileCountH; i++)
+	{
+		for (size_t j = 0; j < tileCountW; j++)
+		{
+			auto& tile = tileData[i][j];
+
+			world_uniform.AssignMatrix4x4(tile.worldMatrix);
+
+			const GLint& texid = tile.textureID;
+
+			texture_uniform.ActiveTexture(texid - 1);
+			texture_uniform.BindTexture(texid);
+
+			model.Render();
+		}
+	}
+}
--- boxhead/boxhead/WorldManager.hpp
+++ boxhead/boxhead/WorldManager.hpp
@@ -1,12 +1,5 @@
 #pragma once
 
-class WorldManagers
-{
-public:
-	void Awake(Scene* scene)
-	{}
-};
-
 struct HeightBlock
 {
 	constexpr HeightBlock(size_t ix, size_t iy, const float& height = 1.0f)
@@ -33,4 +26,75 @@ struct TileCell
 {
 	glm::mat4 worldMatrix;
 	GLint textureID;
+};
+
+class WorldManager
+{
+public:
+	constexpr WorldManager(float xscale, float yscale, float zscale)
+		: heightMap(), tileMap()
+		, stageFilepath("Stage.txt")
+		, boardScaleW(xscale), boardScaleH(yscale), boardScaleD(zscale)
+		, terrainData(), tileData()
+	{
+		heightMap.reserve(boardSizeW * boardSizeH + 1);
+	}
+
+	~WorldManager()
+	{}
+
+	void Awake();
+
+	void Start(Scene* scene);
+
+	void Render(ModelView model, ogl::Uniform& world_uniform, ogl::Uniform& texture_uniform);
+
+private:
+	constexpr HeightBlock& CellAt(const size_t& x, const size_t& y)
+	{
+		const auto pos = x * boardSizeH + y;
+
+		return heightMap.at(pos);
+	}
+
+	constexpr const HeightBlock& CellAt(const size_t& x, const size_t& y) const
+	{
+		const auto pos = x * boardSizeH + y;
+
+		return heightMap.at(pos);
+	}
+
+	constexpr int& SetTerrainAt(const size_t& x, const size_t& y, const int& value)
+	{
+		auto& cell = GetTerrainAt(x, y);
+		cell = value;
+
+		return cell;
+	}
+
+	constexpr int& GetTerrainAt(const size_t& x, const size_t& y)
+	{
+		return terrainData[y][x];
+	}
+
+	constexpr const int& GetTerrainAt(const size_t& x, const size_t& y) const
+	{
+		return terrainData[y][x];
+	}
+
+	static inline constexpr size_t boardSizeW = 40;
+	static inline constexpr size_t boardSizeH = 40;
+	static inline constexpr size_t tileCountW = 20;
+	static inline constexpr size_t tileCountH = 20;
+
+	float boardScaleW;
+	float boardScaleD;
+	float boardScaleH;
+
+	std::vector<HeightBlock> heightMap;
+	std::vector<TileCell> tileMap;
+	int** terrainData;
+	std::string stageFilepath;
+
+	TileCell tileData[tileCountH][tileCountW];
 };
