51c876fac5b6d315b952e70ebe603a34a03b6d09
Iconer <yoyofa2@hotmail.com>
2022-12-13 19:36:07 +0900

    적 생성자 추가

--- boxhead/boxhead/Enemy.hpp
+++ boxhead/boxhead/Enemy.hpp
@@ -3,7 +3,35 @@
 class Enemy : public Entity
 {
 public:
+	template<typename Ty, typename ...ArgTy>
+		requires EnemyType<Ty, ArgTy...>
+	static decltype(auto) Instantiate(ArgTy&& ...args)
+	{
+		return new Ty{ std::forward<ArgTy>(args)... };
+	}
+
 	constexpr Enemy()
 		: Entity()
 	{}
+
+	Enemy(const glm::vec3& position)
+		: Enemy()
+	{
+		SetPosition(position);
+	}
+
+	Enemy(const float& x, const float& y, const float& z)
+		: Enemy()
+	{
+		SetPosition(x, y, z);
+	}
+
+	virtual ~Enemy()
+	{}
+
+	float moveSpeed;
+	float
 };
+
+template<typename Ty, typename ...ArgTy>
+concept EnemyType = std::derived_from<std::decay_t<Ty>, Enemy> && std::constructible_from<std::decay_t<Ty>, ArgTy...>;
